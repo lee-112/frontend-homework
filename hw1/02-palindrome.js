a70e4225e0a642840b1d69a32ba1f08a80e3c952
lee-112 <leeian@pdx.edu>
2023-04-11 23:02:01 -0700

    Added functionality to palindrome.js
    Added EventListener for HTML page input submission.
    
    Added input and output variables to get and set DOM content.
    
    Added isPalindrome function and error checking.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,28 @@
-const elem = document.querySelector('input');
+document.addEventListener('DOMContentLoaded', () => {
+  function isPalindrome(numberInt) {
+    const numberStr = numberInt.toString();
+    const numberStrReversed = numberStr.split('').reverse().join('');
+    return numberStr === numberStrReversed;
+  }
 
-elem.addEventListener('input', handleInput);
+  const inputElement = document.getElementById('inputID');
+  const outputElement = document.getElementById('outputID');
+
+  inputElement.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      const numberStr = inputElement.value;
+      const numberInt = parseInt(numberStr, 10);
+
+      if (Number.isSafeInteger(numberInt) && numberInt >= 0) {
+        const isNumberPalindrome = isPalindrome(numberInt);
+        outputElement.innerHTML = isNumberPalindrome
+          ? '<p class="m-0" style="color: Green;">Yes, This is a Palindrome!</p>'
+          : '<p class="m-0" style="color: FireBrick;">No, Try again.</p>';
+      } else {
+        outputElement.innerHTML = numberInt < 0
+          ? '<p class="alert alert-warning m-0">Error! Only Positive Numbers Allowed.</p>'
+          : '<p class="alert alert-warning m-0">Error! Only Numbers Allowed.</p>';
+      }
+    }
+  });
+});
