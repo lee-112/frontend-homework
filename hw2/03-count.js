c1b97b89aed671ea3260e1cd8519ca0143abe78b
lee-112 <leeian@pdx.edu>
2023-04-25 21:44:21 -0700

    Updated count.js
    Added addEventListener and escapeRegExp functions.

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,31 @@
-// Add your code here
+document.body.style.backgroundColor = '#00435f';
+document.querySelector('main').style.maxWidth = '500px';
 
-input.addEventListener('keydown', handleKeyDown);
+const word = document.getElementById('wordID');
+const text = document.getElementById('textID');
+const innerText = text.innerHTML;
+
+/**
+ * Escapes special characters in a string for regular expressions.
+ * @param string - The input string to escape.
+ * @returns The escaped string with special characters replaced.
+ */
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+/**
+ * Adds an 'input' event listener to the word input element.
+ * When triggered, highlights all word occurrences in the text.
+ */
+word.addEventListener('input', () => {
+  const markWord = word.value;
+  text.innerHTML = innerText;
+
+  if (markWord.trim() !== '') {
+    const matching = RegExp(`\\b${escapeRegExp(markWord)}\\b`, 'gi');
+    text.innerHTML = text.innerHTML.replace(matching, `
+      <mark class="bg-warning d-inline p-0 m-0">${markWord}</mark>
+    `);
+  }
+});
