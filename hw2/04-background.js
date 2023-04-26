e2dbb9e95f4769bef6e4b2a26c3c2c2f657b144e
lee-112 <leeian@pdx.edu>
2023-04-25 21:45:19 -0700

    Updated - background.js
    Added addEventListener and randomColorGenerator functions.

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,36 @@
-// Add your code here
+document.querySelector('main').style.maxWidth = '500px';
+const inputBtn = document.getElementById('buttonID');
+const inputStr = document.getElementById('numberID');
+let interval;
+
+/**
+ * Generates a random color using hsla color values.
+ * Applies generated color to the background of body element.
+ * Background color changes periodically based on user input(seconds).
+ */
+function randomColorGenerator() {
+  const inputInt = (parseInt(inputStr.value, 10) * 1000);
+
+  interval = setInterval(() => {
+    const h = Math.floor(Math.random() * 360);
+    const s = Math.floor(Math.random() * 75);
+    const l = Math.floor(33 - Math.random());
+    const a = ((Math.random() * 0.3) + 0.3);
+    document.body.style.backgroundColor = `
+      hsla(${h}, ${s}%, ${l}%, ${a})`;
+  }, inputInt);
+}
+
+/**
+ * Adds a 'click' event listener to the input button.
+ * When the button is clicked, set/clear interval change.
+ */
+inputBtn.addEventListener('click', () => {
+  if (inputBtn.value === 'Start') {
+    randomColorGenerator();
+    inputBtn.value = 'Stop';
+  } else {
+    clearInterval(interval);
+    inputBtn.value = 'Start';
+  }
+});
