9039f83a47022649ba8b327b4b062a2068a0335a
lee-112 <leeian@pdx.edu>
2023-05-11 14:17:37 -0700

    Updated - hw3/01 Characters
    Added `try/catch` for error handling.
    Used `fetch` to get API data
    Used `Async/Await` to handle promises.
    Used bootstrap & CSS styling.

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,27 @@
-// url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
+async function displayCharacterCards(data) {
+  const sectionTagHTML = document.querySelector('section');
+  data.forEach((character) => {
+    const characterCards = `
+      <div class="card">
+        <img src="${character.imageUrl}">
+        <div class="card-body">
+          <h5 class="fw-bolder">${character.fullName}</h5>
+          <h6 class="fw-bold">${character.title}</h6>
+        </div>
+      </div>`;
+    sectionTagHTML.innerHTML += characterCards;
+  });
+}
 
-fetch(url);
+async function fetchData() {
+  const url = 'https://thronesapi.com/api/v2/Characters';
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    await displayCharacterCards(data);
+  } catch (error) { // eslint-disable-next-line no-console
+    console.error('Error: fetching thronesAPI data', error);
+  }
+}
+
+fetchData();
